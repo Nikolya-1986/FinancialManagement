a01a88df9d5bc07f54d63716110f62cdd56924a4
Nikolai <nikolai_sergeevich@mail.ru>
2025-09-11 13:50:45 +0300

    add RoleSeeder

---
+++ .history/Interfaces/IUserRepository_20250911124720.cs
@@ -0,0 +1,13 @@
+using FinancialManagement.Data;
+
+namespace FinancialManagement.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<ApplicationUser> GetByIdAsync(string id);
+        Task<ApplicationUser> GetByEmailAsync(string email);
+        Task<IEnumerable<ApplicationUser>> GetAllAsync();
+        Task AddUserAsync(ApplicationUser user);
+        Task DeleteUserAsync(string id);
+    }
+}
