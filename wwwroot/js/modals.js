2b62eff95d7df3e3393f6f3304f3324ad4e65f77
Nikolai <nikolai_sergeevich@mail.ru>
2025-09-30 16:01:33 +0300

    modal for roles

---
+++ wwwroot/js/modals.js
@@ -0,0 +1,6 @@
+function openRolesModal(userId, userRole) {
+    document.getElementById('modalUserId').textContent = userId;
+    document.getElementById('modalUserRole').textContent = userRole;
+    var myModal = new bootstrap.Modal(document.getElementById('roleModal'));
+    myModal.show();
+}
